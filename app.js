4eca860772ff8df58df15d776644596a58bd5241
RupeshMax <rupesh.maxpain@gmail.com>
2023-06-24 18:33:16 +0530

    include hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 const app   = express();
@@ -22,10 +22,6 @@ const userSchema =new mongoose.Schema({
     password : String
 });
 
-console.log(process.env.api_key);
-
-
-userSchema.plugin(encrypt,{secret:process.env.secret, encryptedFields: ['password']});
 
 const User = mongoose.model("User",userSchema);
 
@@ -45,7 +41,7 @@ app.get("/register", function(req,res){
 app.post("/register",function(req,res){
     const username = req.body.username;
     console.log(req.body.username);
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     const newUser = new User ({
         email : username,
@@ -64,7 +60,7 @@ app.post("/login",function (req,res) {
 
     const username = req.body.username;
     // console.log(req.body.password);
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email : username}).then(function (foundOne) {
         if (foundOne.password === password){
